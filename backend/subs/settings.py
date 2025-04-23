ea244e6320982d41454b283912806bbf54829a23
Moaz Abdelaziz <moathabdelaziz@outlook.com>
2025-04-23 13:57:56 +0300

    Prepare for production

--- backend/subs/settings.py
+++ backend/subs/settings.py
@@ -60,7 +60,8 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    'http://127.0.0.1:8000'
+    'http://127.0.0.1:8000',
+    'https://buildnow-project-frontend.vercel.app/'
 ]
 
 
